a6a512cf629d70ed7707476f4ac1a49c63ca7633
Pavel Khegay <pashulia.pk@gmail.com>
2022-05-02 19:42:19 +0600

    feat: task Queue

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.end = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    const listNode = new ListNode(value);
+    if (this.end) this.end.next = listNode;
+    this.end = listNode;
+    if (!this.head) this.head = listNode;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (!this.head || this.head === this.tail) {
+      return null;
+    }
+    let headValue = this.head.value;
+    this.head = this.head.next;
+    return headValue;
   }
 }
 
